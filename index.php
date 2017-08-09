fd8a05352b071cf2fff8fb6d3158c927586ba966
udaraseneviratnesmuc <udarachathurangas@gmail.com>
2017-08-09 23:15:44 +0530

    initial commit for the app

---
+++ index.php
@@ -0,0 +1,153 @@
+<html>
+	<head>
+		<title>OBS Database</title>
+		<link href=bootstrap-3.3.7-dist/css/bootstrap.min.css rel=stylesheet>
+		<script src=jquery/jquery-1.12.4.min.js></script>
+		<script src=bootstrap-3.3.7-dist/js/bootstrap.min.js></script>
+	</head>
+
+	<body>
+		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingOne">
+		      <h4 class="panel-title">
+		        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#introduction" aria-expanded="true" aria-controls="collapseOne">
+		          Introduction
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="introduction" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+		      <div class="panel-body">
+		        Goes Introduction
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingTwo">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#medical" aria-expanded="false" aria-controls="collapseTwo">
+		          Medical history
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="medical" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+		      <div class="panel-body">
+		        Goes medical History
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#medication" aria-expanded="false" aria-controls="collapseThree">
+		          Medications
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="medication" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Goes Medications
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#gynObsHistory" aria-expanded="false" aria-controls="collapseThree">
+		          Gyn OBS History
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="gynObsHistory" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Gyn OBS history Goes
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#currentPregnancy" aria-expanded="false" aria-controls="collapseThree">
+		          Current Pregnancy
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="currentPregnancy" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Current Pregnancy Goes here
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#complications" aria-expanded="false" aria-controls="collapseThree">
+		          Complications
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="complications" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Complications
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#diabetes" aria-expanded="false" aria-controls="collapseThree">
+		          Diabetes
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="diabetes" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Diabetes
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#hypertention" aria-expanded="false" aria-controls="collapseThree">
+		          Hypertention
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="hypertention" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Hypertention
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#overview" aria-expanded="false" aria-controls="collapseThree">
+		          Overview
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="overview" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Overview
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingThree">
+		      <h4 class="panel-title">
+		        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#baby" aria-expanded="false" aria-controls="collapseThree">
+		          Baby
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="baby" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		      <div class="panel-body">
+		        Baby
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	</body>
+</html>
